331c207b2c4d202ed5c44bbb6c6e25ec61cb0566
Luis Planella Hernandez <luisplanellah@gmail.com>
2026-02-23 12:32:19 +0100

    inicializado proyecto

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withPreloading, PreloadAllModules, RouteReuseStrategy } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { routes } from './app.routes';
+import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+};
